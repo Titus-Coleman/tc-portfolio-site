e77762a57583d8dc0456acbcd20975946b2da96e
Titus Coleman <dev@tituscoleman.com>
2023-01-02 17:57:00 -0500

    added skill icons and hexagon tweaks

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,7 +10,7 @@ function About(props: Props) {
     <section 
       id='about'
       className={styles.aboutContainer}>
-      <h2 className={styles.aboutTitle}>About</h2>
+      <h3 className={styles.aboutTitle}>About</h3>
       <div className={styles.aboutText}>
         <p>
         I started my tech career as a Sys Admin getting familiar with PHP and SQL on the job. After a few too many nights in cold server rooms and numerous DDoS attacks, I ventured outside of tech professionally, but I never truly left in my personal life. I realized my passion was to make an <span>impact</span> but also have fun in my career, so I learned to code.
