1462958d11bdc02a749b8c33b8c08ca6a79797f2
Titus Coleman <dev@tituscoleman.com>
2023-01-13 17:35:35 -0500

    card design completed

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion'
+import Image from 'next/image';
 import { useRef, useState, useEffect } from 'react'
 import styles from '../styles/Projects.module.css'
 import { Project } from '../typings'
@@ -17,28 +18,36 @@ function Projects({ projects }: Props) {
       <div className={styles.slide__container}>
           <div className={styles.slide__content}>
             <div className={styles.card__wrapper}>
-              <div className={styles.card}>
-                
-                <div className={styles.image__content}>
+            {projects?.map((pCard, i) => (
+              <article className={styles.card}>
+      
+                <header className={styles.card__header}>
+                  <h5>{i+1} of {projects.length}</h5>
+                <h4 className={styles.card__name}>{pCard?.title}</h4>
+                </header>
+                <picture className={styles.image__content}>
                   <span className={styles.overlay}></span>
                   <div className={styles.card__image}>
-                    <img 
+                    <Image 
                         className={styles.card__img}
-                        src="app-mockup.svg" 
-                        alt=""  />
+                        src={pCard.image.secure_url}
+                        alt=""
+                        width={320} 
+                        height={200}/>
                   </div>
-                </div>
-                <div className={styles.card__content}>
-                  <h4 className={styles.card__name}>Project Name Here</h4>
-                  <p className={styles.card__description}>Description</p>
+                </picture>
+                <div className={styles.card__content}>                  
+                  <p className={styles.card__description}>{pCard?.summary}</p>
                   <div className={styles.card__techstack}>
-                      <span className={styles.card__tag}>Javascript</span>
-                      <span className={styles.card__tag}>Rust</span>
-                      <span className={styles.card__tag}>React</span>
+                  {pCard?.technologies?.map((tech) => (
+                      <span key={tech?._id} className={styles.card__tag}>{tech?.title}</span>
+
+                  ))}                  
                   </div>
-                  <div className={styles.card__links}>
+                  
+                  <footer className={styles.card__links}>
                       <a 
-                        // href={pCard.linkToSite}
+                        href={pCard.linkToSite}
                         target={'_blank'}>
                         <img
                             className={styles.card__site}
@@ -49,7 +58,7 @@ function Projects({ projects }: Props) {
                             />
                       </a>
                       <a 
-                        // href={pCard.linkToGitHub}
+                        href={pCard.linkToGitHub}
                         target={'_blank'}>
                         <img
                             className={styles.card__git}
@@ -59,9 +68,10 @@ function Projects({ projects }: Props) {
                             height={35}
                             />
                       </a>
-                  </div>
+                  </footer>
                 </div>
-              </div>
+              </article>
+              ))} 
             </div>
           </div>
       </div>
