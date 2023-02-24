5081564aabd40bb4c29ec2905ab127a270f920f1
Titus Coleman <dev@tituscoleman.com>
2023-02-23 21:41:35 -0500

    final edits

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## Welcome
 
-Thank you for taking the time to review the source code of my portfolio site. I've used the following to help build what you see on [TitusColeman.com](https://tituscoleman.com):
+Thank you for taking the time to review the source code of my portfolio site. I've used the following to help build what you see on [TitusColeman.dev](https://tituscoleman.dev:
 
 [Next.js](https://nextjs.org/)
-[CSS]
-[TypeScript]
+CSS
 
+TypeScript
+
+Sanity CMS
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,68 +1,61 @@
-import Image from 'next/image'
-import React from 'react'
-import styles from '../styles/Navbar.module.css'
-import { SocialIcon } from 'react-social-icons'
-import Link from 'next/link'
-import { motion } from 'framer-motion'
+import Image from 'next/image';
+import React from 'react';
+import styles from '../styles/Navbar.module.css';
+import { SocialIcon } from 'react-social-icons';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-        <motion.div 
-            className={styles.email}
-            initial={{
-                x: -500,
-                opacity: 0,
-                scale: 0.5,
-            }}
-            animate={{
-                x:0,
-                opacity: 1,
-                scale: 1
-            }}
-            transition={{ duration: 1.0 }}
-            >
-            <Link href={"/example.pdf"} target={"_blank"}>
-            <button className={styles.resume_btn}>
-                resume    
-            </button>
-            </Link>
-                 
-        </motion.div>
+      <motion.div
+        className={styles.email}
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{ duration: 1.0 }}
+      >
+        <Link href={'/Titus_Coleman_Resume.pdf'} target={'_blank'}>
+          <button className={styles.resume_btn}>resume</button>
+        </Link>
+      </motion.div>
 
-        <motion.div 
-            className={styles.socials}
-            initial={{
-                x: 500,
-                opacity: 0,
-                scale: 0.5,
-            }}
-            animate={{
-                x:0,
-                opacity: 1,
-                scale: 1
-            }}
-            transition={{ duration: 1.0 }}>
-            <SocialIcon 
-                    url='#contact'
-                    network='email' 
-                    fgColor="gray"
-                    bgColor="transparent"/>
-            <SocialIcon 
-                url='https://github.com/Titus-Coleman' 
-                target={"_blank"}
-                fgColor="gray"
-                bgColor="transparent"/>
-            {/* <SocialIcon 
+      <motion.div
+        className={styles.socials}
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{ duration: 1.0 }}
+      >
+        <SocialIcon url="#contact" network="email" fgColor="gray" bgColor="transparent" />
+        <SocialIcon
+          url="https://github.com/Titus-Coleman"
+          target={'_blank'}
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        {/* <SocialIcon 
                 url='https://www.linkedin.com/in/tituscoleman/' 
                 target={"_blank"}
                 fgColor="gray"
                 bgColor="transparent"/> */}
-        
-        </motion.div>
-
+      </motion.div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
